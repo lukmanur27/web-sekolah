8f0a38277edb7a9e9194ab29f3589f367f2d0eab
lukmanur27 <85560259+lukmanur27@users.noreply.github.com>
2021-06-08 23:23:51 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# web-sekolah
-source code dari tugas akhir matkul pemrograman web PTI 2019 A kelompok 1
+# Web Sekolah Sederhana Dengan PHP & MySQL 
+
+
+Ini merupakan tugas kuliah akhir mata kuliah pemrograman web kelompok 1 PTI 2019 A yang beranggotakan
+1.Lukman Nur Faizin 19050974002
+2.Ahshoha Qori'a Zulda 19050974014
+3.Galang Fajar Samudro 19050974033
+
+
+
+
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php 
+
+require_once 'koneksi.php';
+$query = mysqli_query($koneksi, "SELECT * FROM tbl_artikel");
+
+$aktif = 'home';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Home - SMK Negeri 13 Malang</title>
+	<link rel="stylesheet" href="resources/fonts/stylesheet.css">
+	<link rel="stylesheet" href="resources/css/bootstrap.min.css">
+	<link rel="stylesheet" href="resources/css/style.css">
+	<link href="resources/images/logo.png" rel="shortcut icon">
+</head>
+
+<body>
+	<div class="container bg-light">
+		<!-- top bar -->
+		<div class="logo clearfix">
+			<div class="float-left mt-3 mb-3">
+				<img src="resources/images/logo.png" alt="Logo Sekolah" width="70px" class="float-left mr-3">
+				<div class="text float-right">
+					<span class="smk">SMK Negeri 13 MALANG</span><br>
+					<span class="visi">Jl. Villa Bukit Tidar No. 13, Merjosari, Kec.Lowokwaru, Malang.</span>
+				</div>
+			</div>
+		</div>
+			
+		<!-- nav bar -->
+		<?php require_once 'navbar.php'; ?>
+
+		<div id="slider">
+		<figure>
+			<img src="resources/images/banner_1.jpg">
+			<img src="resources/images/banner_2.jpg">
+			<img src="resources/images/banner_3.jpg">
+			<img src="resources/images/banner_4.jpg">
+			<img src="resources/images/banner_5.jpg">
+		</figure>
+	    </div>
+		
+		<!-- content -->
+		<div class="row p-3">
+			<div class="col-md-8">
+				<div class="title mb-3">
+					Informasi Terbaru
+				</div>
+				<?php while($artikel = mysqli_fetch_assoc($query)) : ?>
+				<div class="artikel">
+					<div class="info">
+						<div class="post mb-2">
+							<span class="tgl"><?= date('d M y', strtotime($artikel['tanggal'])) ?></span>
+							<span class="judul"><a href="detail_artikel.php?id=<?= $artikel['id'] ?>"><?= $artikel['judul'] ?></a></span>
+						</div>
+					</div>
+					<div class="detail">
+						<div class="clearfix" style="text-align: justify;">
+							<img src="images/artikel/<?= $artikel['foto'] ?>" alt="" width="200px" class="float-left mr-2">
+							<?= substr($artikel['isi'], 0, 150) . '...' ?>
+						</div>
+					</div>
+					<hr>
+				</div>
+				<?php endwhile; ?>
+			</div>
+			<?php require_once 'sidebar.php'; ?>
+		</div>
+		<div class="text-white footer">
+			2021 © Copyright by Kelompok 1 PTI19 A.
+		</div>
+	</div>
+
+	<script src="resources/js/jquery.js"></script>
+	<script src="resources/js/bootstrap.min.js"></script>
+</body>
+</html>
