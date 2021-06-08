6c7e100e6d61159e005dd9e6c5a7f20d12064a1d
lukmanur27 <85560259+lukmanur27@users.noreply.github.com>
2021-06-08 23:36:53 +0700

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,37 @@
+<?php 
+require_once 'cek_session.php';
+$active = 'dashboard';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Halaman Admin - SMK Negeri MALANG</title>
+	<link rel="stylesheet" href="../resources/css/bootstrap.min.css">
+</head>
+<body>
+	<?php require_once 'navbar.php'; ?>
+	<div class="container mt-3">
+		<div class="row">
+			<div class="col">
+				<div class="card shadow">
+					<div class="card-header">Halaman Admin!</div>
+					<div class="jumbotron">
+                        <div class="container">
+                        <h1 class="display-3">SELAMAT DATANG </h1>
+                        <p class="lead">Selamat bekerja ! Gunakan sistem ini untuk kepentingan pengelolaan web SMKN 13 MALANG. Mari ciptakan web yang responsive serta berkualitas</p>
+                        <p>Kelompok 1</p>
+                        </div>
+                   </div>
+                   </div>
+                </div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="../resources/js/jquery.js"></script>
+	<script src="../resources/js/bootstrap.min.js"></script>
+</body>
+</html>
